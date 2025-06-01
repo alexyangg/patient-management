238b88bd34cf070e5a44c2e21e74f2638f998ccd
Alex Yang <alexemail67@gmail.com>
2025-05-31 22:22:10 -0700

    create billing service microservice

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,34 @@
+package com.pm.billingservice.grpc;
+
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+                                     StreamObserver<BillingResponse> responseObserver) {
+
+        log.info("createBillingAccount request received: {}", billingRequest.toString());
+
+        // Business logic - e.g save to database, perform calculations, etc.
+
+        // dummy data to simulate billing an account
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response); // send response from grpc service back to client
+
+        // response completed; end cycle in this response
+        responseObserver.onCompleted(); // (we could have sent multiple responses using onNext())
+
+    }
+}
