86c12dd5895ceac22bd6e5a22a16cadf50965383
Alex Yang <alexemail67@gmail.com>
2025-06-08 23:35:25 -0700

    implement /validate endpoint

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -3,6 +3,8 @@ package com.pm.authservice.service;
 import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.model.User;
 import com.pm.authservice.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -24,11 +26,20 @@ public class AuthService {
         Optional<String> token = userService
                 // find user by email
                 .findByEmail(loginRequestDTO.getEmail())
-                // if user has a valud, check passwords against database to ensure they match
+                // if user has a value, check passwords against database to ensure they match
                 .filter(user -> passwordEncoder.matches(loginRequestDTO.getPassword(), user.getPassword()))
                 // if passwords match, convert User object to a String token
                 .map(user -> jwtUtil.generateToken(user.getEmail(), user.getRole()));
 
         return token;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            jwtUtil.validateToken(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
     }
 }
