6b4486b8b7b22b2db0c533b25ff186caa5c00fc3
Alex Yang <alexemail67@gmail.com>
2025-07-09 15:41:30 -0700

    create Virtual Private Cloud (VPC)

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -1,11 +1,24 @@
 package com.pm.stack;
 
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.Vpc;
 
 public class LocalStack extends Stack {
+    private final Vpc vpc;
+
     // boilerplate code to create a new stack
     public LocalStack(final App scope, final String id, final StackProps props) {
-        super(scope, id, props);
+        super(scope, id, props); // scope is the stack we're currently in
+
+        this.vpc = createVpc();
+    }
+
+    // VPC creates routing and networks required for our internal services to communicate with each other
+    private Vpc createVpc() {
+        return Vpc.Builder.create(this, "PatientManagementVPC")
+                .vpcName("PatientManagementVPC")
+                .maxAzs(2) // max 2 availability zones
+                .build();
     }
 
     public static void main(final String[] args) {
