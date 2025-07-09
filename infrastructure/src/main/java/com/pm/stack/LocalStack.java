fb1507dc7bee30a807d46a2cbcbf3d2a7089223a
Alex Yang <alexemail67@gmail.com>
2025-07-09 16:39:24 -0700

    create AWS Database System (DBS)

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -1,7 +1,11 @@
 package com.pm.stack;
 
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.InstanceClass;
+import software.amazon.awscdk.services.ec2.InstanceSize;
+import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.rds.*;
 
 public class LocalStack extends Stack {
     private final Vpc vpc;
@@ -11,6 +15,9 @@ public class LocalStack extends Stack {
         super(scope, id, props); // scope is the stack we're currently in
 
         this.vpc = createVpc();
+
+        DatabaseInstance authServiceDb = createDatabase("AuthServiceDB", "auth-service-db");
+        DatabaseInstance patientServiceDb = createDatabase("PatientServiceDB", "patient-service-db");
     }
 
     // VPC creates routing and networks required for our internal services to communicate with each other
@@ -18,6 +25,25 @@ public class LocalStack extends Stack {
         return Vpc.Builder.create(this, "PatientManagementVPC")
                 .vpcName("PatientManagementVPC")
                 .maxAzs(2) // max 2 availability zones
+                .build();
+    }
+
+    private DatabaseInstance createDatabase(String id, String dbName) {
+        // builder pattern
+        return DatabaseInstance.Builder
+                .create(this, id)
+                .engine(DatabaseInstanceEngine.postgres(
+                        PostgresInstanceEngineProps.builder()
+                                .version(PostgresEngineVersion.VER_17_2)
+                                .build()
+                        )
+                )
+                .vpc(vpc)
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO)) // specify compute levels
+                .allocatedStorage(20) // amount of storage allocated to this database
+                .credentials(Credentials.fromGeneratedSecret("admin_user"))
+                .databaseName(dbName)
+                .removalPolicy(RemovalPolicy.DESTROY) // remove database storage when stack is removed. in prod, need to specify backup policy
                 .build();
     }
 
